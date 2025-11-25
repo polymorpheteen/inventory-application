77f5ea023cce777790caf5bdeebb605878f61d1f
polymorpheteen <138623315+polymorpheteen@users.noreply.github.com>
2025-11-25 01:37:00 +0100

    Fix updateCover.js and add it as a module to use.

--- updateCover.js
+++ updateCover.js
@@ -2,13 +2,24 @@ require("dotenv").config(); // load API key
 const { getCoverUrl } = require("./services/lastfm");
 const { updateCoverUrl } = require("./db/queries");
 
+/**
+ * Fetches the album cover from Last.fm and updates the database.
+ * @param {number} albumId
+ * @param {string} artist
+ * @param {string} albumTitle
+ * @returns
+ */
 async function updateAlbumCover(albumId, artist, albumTitle) {
-  const coverUrl = await getCoverUrl(artist, albumTitle);
-  if (!coverUrl) return console.log("Cover not found");
+  try {
+    const coverUrl = await getCoverUrl(artist, albumTitle);
+    if (!coverUrl) return console.log("Cover not found:", artist, albumTitle);
 
-  const updated = await updateCoverUrl(albumId, coverUrl);
-  console.log("Updated album:", updated);
+    const updated = await updateCoverUrl(albumId, coverUrl);
+    console.log("Updated album:", updated);
+  } catch (err) {
+    console.error("Error Updating cover:", err);
+    throw err;
+  }
 }
 
-// Run the function
-updateAlbumCover(3, "Kendrick Lamar", "good kid, m.A.A.d city");
+module.exports = { updateAlbumCover };
